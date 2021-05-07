9e4970f655da78e0fce4b4590926d51ae24753e5
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-05-07 00:25:26 -0700

    Check in bridge and server

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ from lume_epics.epics_server import Server
 from model import AccModel
 
 if __name__ == '__main__':
+    print("Starting channel access server...")
     server = Server(
         AccModel,
-        "BMAD",
-        protocols = ["ca", "pva"]
+        "test",
+        protocols = ["ca"],
     )
 
     server.start(monitor=True)
+    print("Server stopped.")

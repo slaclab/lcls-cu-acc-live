d3a037de45b346e89beafd2ef76558b299cf2cec
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-05-14 14:38:09 -0700

    Set up logging for server debug

--- server.py
+++ server.py
@@ -1,8 +1,39 @@
 from lume_epics.epics_server import Server
 from model import AccModel
+import argparse
+import logging
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-log", 
+    "--log", 
+    default="warning",
+    help=(
+        "Provide logging level. "
+        "Example --log debug', default='warning'"),
+)
+
+options = parser.parse_args()
+levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARNING,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
+level = levels.get(options.log.lower())
+if level is None:
+    raise ValueError(
+        f"log level given: {options.log}"
+        f" -- must be one of: {' | '.join(levels.keys())}")
+logging.basicConfig(level=level)
+logger = logging.getLogger("lume-epics")
 
 if __name__ == '__main__':
-    print("Starting channel access server...")
+    import os
+    import sys
+    print("Starting server...")
     server = Server(
         AccModel,
         "test",
